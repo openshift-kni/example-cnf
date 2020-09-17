b95b8c389394c83e9a06478593a659d2f1dc4312
Saravanan KR <skramaja@redhat.com>
2020-09-17 22:34:29 +0530

    Added custom mac workaround image with sirov-cni executable

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ REGISTRY="quay.io/krsacme"
 TAG="${TAG:-v0.1.0}"
 NAME=${REGISTRY}"/testpmd-container-app:"${TAG}
 docker build . -t $NAME && docker push $NAME
+
+CNI_IMG_NAME=${REGISTRY}"/testpmd-container-app-mac-fix:"${TAG}
+docker build sriov-cni --file sriov-cni/Dockerfile -t $CNI_IMG_NAME && docker push $CNI_IMG_NAME
