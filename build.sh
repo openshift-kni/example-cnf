2b409ee44b01228fcb32247e9f5d13f1d691aeb9
Saravanan KR <skramaja@redhat.com>
2020-09-23 09:30:24 +0530

    Fix testpmd run and configure command

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ TAG="${TAG:-v0.1.1}"
 NAME=${REGISTRY}"/testpmd-container-app:"${TAG}
 docker build . -t $NAME && docker push $NAME
 
-CNI_IMG_NAME=${REGISTRY}"/testpmd-container-app-mac-fix:"${TAG}
-docker build sriov-cni --file sriov-cni/Dockerfile -t $CNI_IMG_NAME && docker push $CNI_IMG_NAME
+if [[ $1 == "all" ]]; then
+    CNI_IMG_NAME=${REGISTRY}"/testpmd-container-app-mac-fix:"${TAG}
+    docker build sriov-cni --file sriov-cni/Dockerfile -t $CNI_IMG_NAME && docker push $CNI_IMG_NAME
+fi
