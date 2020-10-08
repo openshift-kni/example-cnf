4b878d768fc1f5636ab6d42729d44fe6a57ce21e
Saravanan KR <skramaja@redhat.com>
2020-10-08 16:54:41 +0530

    Bump to version v0.1.3

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CLI=docker
 REGISTRY="quay.io/krsacme"
-TAG="${TAG:-v0.1.2}"
+TAG="${TAG:-v0.1.3}"
 NAME=${REGISTRY}"/trex-container-app:"${TAG}
 $CLI build . -t $NAME && $CLI push $NAME
