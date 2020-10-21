dd0604b37f96a06d812dcebdf9de5ae5ab4725c4
Saravanan KR <skramaja@redhat.com>
2020-10-21 09:14:32 +0530

    v0.2.0: Separated server and application containers

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 CLI=docker
 REGISTRY="quay.io/krsacme"
-TAG="${TAG:-v0.1.3}"
+TAG="${TAG:-v0.2.0}"
+
+if [[ $1 == "all" ]]; then
+    NAME=${REGISTRY}"/trex-container-server:"${TAG}
+    $CLI build server -f server/Dockerfile -t $NAME
+    $CLI push $NAME
+fi
+
 NAME=${REGISTRY}"/trex-container-app:"${TAG}
-$CLI build . -t $NAME && $CLI push $NAME
+$CLI build app -f app/Dockerfile -t $NAME
+$CLI push $NAME
