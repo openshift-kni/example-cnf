6368cff28ad4c394bdd6bb68a95f7067aa714af4
Saravanan KR <skramaja@redhat.com>
2020-10-21 18:57:01 +0530

    Added competed stats when the test is run for duration

--- app/pyfiles/trexstats.py
+++ app/pyfiles/trexstats.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from threading import Thread
 
@@ -66,3 +67,45 @@ class TRexAppStats(object):
             Thread(target=trexevent.create_event, args=[data]).start()
             self.event_notified_miss = miss
 
+
+def completed_stats(stats, warnings, port_a, port_b):
+    packet_size = os.getenv("PACKET_SIZE") or os.getenv("packet_size") or 64
+    packet_rate = os.getenv("PACKET_RATE") or os.getenv("packet_rate") or "10pps"
+    duration = os.getenv("DURATION") or os.getenv("duration") or -1
+
+    data = {}
+    now = datetime.now()
+    data['microtime'] = now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    data['time'] = now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    data['msg'] = ("Packet (%s) with rate (%s) for (%s) seconds "
+        "have completed" % (packet_size, packet_rate, duration))
+    data['reason'] = 'TestCompleted'
+    trexevent.create_event(data)
+
+    passed = False
+    log.info(json.dumps(stats[port_a], indent = 4, separators=(',', ': '), sort_keys = True))
+    log.info(json.dumps(stats[port_b], indent = 4, separators=(',', ': '), sort_keys = True))
+
+    lost_a = stats[port_a]["opackets"] - stats[port_b]["ipackets"]
+    lost_b = stats[port_b]["opackets"] - stats[port_a]["ipackets"]
+
+    log.info("\npackets lost from {0} --> {1}:   {2} pkts".format(port_a, port_b, lost_a))
+    log.info("packets lost from {0} --> {1}:   {2} pkts".format(port_b, port_a, lost_b))
+
+    if warnings:
+        log.info("\n\n*** test had warnings ****\n\n")
+        for w in warnings:
+            log.info(w)
+
+    if (lost_a == 0) and (lost_b == 0) and not warnings:
+        passed = True
+        packets = stats[port_a]["opackets"] + stats[port_b]["opackets"]
+        data['msg'] = ("Test has Passed with no loss, total packets %s" % packets)
+        data['reason'] = 'TestPassed'
+    else:
+        data['msg'] = ("Test has failed with packets loss, (%s, %s) "
+            "packets lost on each port" % (lost_a, lost_b))
+        data['reason'] = 'TestFailed'
+
+    trexevent.create_event(data)
+    return passed
