f39a538b30045ce187e96c55cac151774f75e030
Frederic Lepied <flepied@redhat.com>
2023-06-06 18:54:54 +0200

    generate-versions.sh: force to build operators when an app is built

--- generate-versions.sh
+++ generate-versions.sh
@@ -14,19 +14,41 @@ DIRS="testpmd-container-app trex-container-app cnf-app-mac-operator testpmd-lb-o
 
 FILES="$(git diff --name-only -r origin/${GITHUB_BASE_REF:-main}:)"
 
+TESTPMD_APP=false
+TREX_APP=false
+
 echo "declare -A VERSIONS" > "$VERSIONS"
 
 count=0
 for d in $DIRS; do
     vers="$(cd "$d" || exit 1; make -s version)"
+    # Force to build the operators if the corresponding app is built
+    force=false
+    case "$d" in
+        testpmd-lb-operator|testpmd-operator)
+            if [ "$TESTPMD_APP" == true ]; then
+                force=true
+            fi
+            ;;
+        trex-operator)
+            if [ "$TREX_APP" == true ]; then
+                force=true
+            fi
+            ;;
+    esac
     # Force nfv-example-cnf-index to be displayed if at least one
     # change to the subdirs has been detected. This will force the
-    # index to be generated. TODO: force the operators to be rebuilt
-    # if there is a change in the apps.
-    if [ "$FORCE_BUILD" == true ] || grep -q "$d/" <<< "$FILES" || { [ "$d" == nfv-example-cnf-index ] && [ "$count" -gt 0 ]; }; then
+    # index to be generated.
+    if [ "$force" == true ] || [ "$FORCE_BUILD" == true ] || grep -q "$d/" <<< "$FILES" || { [ "$d" == nfv-example-cnf-index ] && [ "$count" -gt 0 ]; }; then
         echo "VERSIONS[$d]=$vers-$EXTRA" >> "$VERSIONS"
         echo "$d"
         count=$((count + 1))
+        if [ "$d" == testpmd-container-app ]; then
+            TESTPMD_APP=true
+        fi
+        if [ "$d" == trex-container-app ]; then
+            TREX_APP=true
+        fi
     else
         echo "VERSIONS[$d]=$vers" >> "$VERSIONS"
     fi
