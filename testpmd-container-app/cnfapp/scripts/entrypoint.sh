039be212f8cb8e784c81cf38dcf18e01f584a117
Ramon Perez <raperez@redhat.com>
2024-01-18 09:54:08 +0100

    improve webserver management, including copy from utils directory, and also improve copy process in dockerfile

--- testpmd-container-app/cnfapp/scripts/entrypoint.sh
+++ testpmd-container-app/cnfapp/scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # Start lifecycle webserver in background
-/bin/bash /usr/local/bin/webserver 8095 &
+/usr/local/bin/webserver 8095 &
 
 # Call to testpmd/run
 /bin/bash /var/lib/testpmd/run
