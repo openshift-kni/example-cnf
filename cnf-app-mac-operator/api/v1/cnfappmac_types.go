9c74ffbc23a673cb41788ffb2ac25b13b2fc3cbd
Ramon Perez <raperez@redhat.com>
2024-12-12 15:42:18 +0100

    Add cnf-app-mac-operator

--- cnf-app-mac-operator/api/v1/cnfappmac_types.go
+++ cnf-app-mac-operator/api/v1/cnfappmac_types.go
@@ -41,8 +41,8 @@ type CNFAppMacSpec struct {
 type CNFAppMacStatus struct {
 }
 
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
 
 // CNFAppMac is the Schema for the cnfappmacs API
 type CNFAppMac struct {
@@ -53,7 +53,7 @@ type CNFAppMac struct {
 	Status CNFAppMacStatus `json:"status,omitempty"`
 }
 
-//+kubebuilder:object:root=true
+// +kubebuilder:object:root=true
 
 // CNFAppMacList contains a list of CNFAppMac
 type CNFAppMacList struct {
